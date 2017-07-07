0d5b164e08e3cbecbf7ebdcde03eca20be35ee46
Janic Heen <janic@online.no>
2017-07-07 22:04:21 +0200

    Added endpoints

--- TestREST_Framework/tutorial/snippets/views.py
+++ TestREST_Framework/tutorial/snippets/views.py
@@ -11,6 +11,13 @@ from snippets.serializers import UserSerializer
 
 from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+from rest_framework import renderers
+
 # Create your views here.
 
 class SnippetList(generics.ListCreateAPIView):
@@ -34,4 +41,17 @@ class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-#legger til en linje
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
