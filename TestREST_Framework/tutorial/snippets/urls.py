72d270cc2cef432f60c0a4259e9ede5597576933
Janic Heen <janic@online.no>
2017-07-07 22:30:17 +0200

    Added hyperlinking

--- TestREST_Framework/tutorial/snippets/urls.py
+++ TestREST_Framework/tutorial/snippets/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
@@ -10,6 +10,12 @@ urlpatterns = [
 	url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view(), name='snippet-highlight'),
 	url(r'^users/$', views.UserList.as_view(), name='user-list'),
 	url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail'),	
+]
+
+# Login and logout views for the browsable API
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
