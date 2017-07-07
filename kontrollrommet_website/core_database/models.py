da46c66a0be6499a28622ffd8431d8b4d1af70cf
Janic Heen <janic@online.no>
2017-07-08 01:22:24 +0200

    Implemented User field in Person model
    user relates to User model,
    Added code to automatically fix user to Person, when user i created og edited.
    PS! Haven't tested it yeat

--- kontrollrommet_website/core_database/models.py
+++ kontrollrommet_website/core_database/models.py
@@ -2,6 +2,9 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 ### Category lists### 
@@ -18,7 +21,18 @@ class Person(models.Model):
 	#id = models.AutoField(primary_key=True)
 	first_name = models.CharField(max_length=50)
 	last_name = models.CharField(max_length=50)
-	isuser = models.BooleanField(default=False)
+	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+
+# 
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Person.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.person.save()
+
 # Entity index
 class Entity(models.Model):
 	#id = models.AutoField(primary_key=True)
