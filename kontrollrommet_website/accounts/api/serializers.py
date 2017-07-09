9bf7e0c98800f3bcbe6226c74b1bdbc6494a621f
Janic Heen <janic@online.no>
2017-07-09 03:04:29 +0200

    Might updated old code example, but
    This is not tested, and looks faulty.

--- kontrollrommet_website/accounts/api/serializers.py
+++ kontrollrommet_website/accounts/api/serializers.py
@@ -9,9 +9,9 @@ class UserSerializer(serializers.ModelSerializer):
         write_only_fields = ('password',)
         read_only_fields = ('is_staff', 'is_superuser', 'is_active', 'date_joined',)
  
-    def restore_object(self, attrs, instance=None):
+    def update(self, attrs, instance=None):
         # call set_password on user object. Without this
         # the password will be stored in plain text.
-        user = super(UserSerializer, self).restore_object(attrs, instance)
+        user = super(UserSerializer, self).update(attrs, instance)
         user.set_password(attrs['password'])
         return user
