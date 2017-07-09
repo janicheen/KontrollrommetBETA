7bc46d895d00726c05a385a99623b18e4e04f468
Janic Heen <janic@online.no>
2017-07-09 11:15:17 +0200

    Added Person fields into serializer - WORKING!
    Not sure if update and creat are working yet thouhg

--- kontrollrommet_website/accounts/api/serializers.py
+++ kontrollrommet_website/accounts/api/serializers.py
@@ -3,15 +3,18 @@ from rest_framework import serializers
  
  
 class UserSerializer(serializers.ModelSerializer):
+    first_name = serializers.CharField(source='person.first_name')
+    last_name = serializers.CharField(source='person.last_name')
+
     class Meta:
         model = User
-        fields = ('password', 'first_name', 'last_name', 'email',)
+        fields = ('username', 'password', 'email', 'first_name', 'last_name')
         write_only_fields = ('password',)
         read_only_fields = ('is_staff', 'is_superuser', 'is_active', 'date_joined',)
  
-    def update(self, attrs, instance=None):
+    def restore_object(self, attrs, instance=None):
         # call set_password on user object. Without this
         # the password will be stored in plain text.
-        user = super(UserSerializer, self).update(attrs, instance)
+        user = super(UserSerializer, self).restore_object(attrs, instance)
         user.set_password(attrs['password'])
         return user
