cd43b050491353a7ac49bd2082f78776fe1ea6fe
Janic Heen <janic@online.no>
2017-07-12 17:35:18 +0200

    Finally breaktrhu in display lists of participants

--- kontrollrommet_website/meeting_manager/serializers.py
+++ kontrollrommet_website/meeting_manager/serializers.py
@@ -1,5 +1,12 @@
-from meeting_manager.models import Meeting, Entity, Person
+from meeting_manager.models import Meeting, Entity, Person, Participant
 from rest_framework import serializers
+
+class ParticipantSerializer(serializers.ModelSerializer):
+	id = serializers.ReadOnlyField(source='person.id')
+	name = serializers.ReadOnlyField(source='person.first_name')
+	class Meta:
+		model = Participant
+		fields = ('id', 'name', 'is_invited')
 
 class PersonSerializer(serializers.ModelSerializer):
 	class Meta:
@@ -7,9 +14,12 @@ class PersonSerializer(serializers.ModelSerializer):
 		fields = ('id', 'first_name', 'last_name', 'user')
 
 class MeetingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Meeting
-        fields = ('id', 'meeting_category', 'entity', 'requested_meetdate', 'participants')
+	participants = ParticipantSerializer(source='participant_set', many=True)
+	#    participants = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+	#    print(participants)
+	class Meta:
+		model = Meeting
+		fields = ('id', 'meeting_category', 'entity', 'requested_meetdate', 'participants')
 
 class EntitySerializer(serializers.ModelSerializer):
 	class Meta:
