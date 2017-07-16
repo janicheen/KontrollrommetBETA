e953b6df2ef1de1a737474781966cfacaec6197e
Janic Heen <janic@online.no>
2017-07-16 21:14:59 +0200

    Replaces all files with prepared files Angular
    Now working through HTTP chapter

--- kontrollrommet_website/angular/quickstart-master/src/app/app.module.ts
+++ kontrollrommet_website/angular/quickstart-master/src/app/app.module.ts
@@ -1,30 +1,28 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { RouterModule }   from '@angular/router';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
 
-import { AppComponent }  from './app.component';
-import { DashboardComponent }     from './dashboard.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent }     from './heroes.component';
-import { HeroService }         from './hero.service';
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroService }          from './hero.service';
 
 import { AppRoutingModule }     from './app-routing.module';
 
-
 @NgModule({
-  imports:      [ 
+  imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule
   ],
-  declarations: [ 
+  declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent,
-    DashboardComponent
+    HeroesComponent
   ],
-  providers:  [ HeroService ],
-  bootstrap:    [ AppComponent ]
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
