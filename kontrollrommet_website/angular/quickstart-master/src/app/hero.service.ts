ea685454544d45aed4b226bd0ffbe20a909af49e
Janic Heen <janic@online.no>
2017-07-16 22:15:04 +0200

    Implemented http functionality for tutorial

--- kontrollrommet_website/angular/quickstart-master/src/app/hero.service.ts
+++ kontrollrommet_website/angular/quickstart-master/src/app/hero.service.ts
@@ -1,13 +1,29 @@
-import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import { Hero } from './hero';
 
 @Injectable()
 export class HeroService {
-  getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
-  }
+  private heroesUrl = 'api/heroes';  // URL to web api
+  private headers = new Headers({'Content-Type': 'application/json'});
 
+  constructor(private http: Http) { }
+ 
+  getHeroes(): Promise<Hero[]> {
+    return this.http.get(this.heroesUrl)
+      .toPromise()
+      .then(response => response.json().data as Hero[])
+      .catch(this.handleError);
+  }
+ 
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+  
   getHeroesSlowly(): Promise<Hero[]> {
     return new Promise(resolve => {
       // Simulate server latency with 2 second delay
@@ -16,7 +32,20 @@ export class HeroService {
   }
 
   getHero(id: number): Promise<Hero> {
-    return this.getHeroes()
-               .then(heroes => heroes.find(hero => hero.id === id));
+  const url = `${this.heroesUrl}/${id}`;
+  return this.http.get(url)
+    .toPromise()
+    .then(response => response.json().data as Hero)
+    .catch(this.handleError);
   }
+
+  update(hero: Hero): Promise<Hero> {
+  const url = `${this.heroesUrl}/${hero.id}`;
+  return this.http
+    .put(url, JSON.stringify(hero), {headers: this.headers})
+    .toPromise()
+    .then(() => hero)
+    .catch(this.handleError);
+  }
+  
 }
