04fdcead38c87ece5fb17823aac5d74be1256fa3
Janic Heen <janic@online.no>
2017-08-29 23:13:15 +0200

    Implemented bootstrap column division in meeting request form

--- angular-frontend/kontrollrommet-frontend/src/app/_services/data.service.ts
+++
@@ -1,8 +0,0 @@
-
-
-setCurrentUser() {
-        this.authService.getCurrentUser()
-        .then(user => this.user = user);
-        console.log("I am running respons to login!", this.user);
-}
-    
---
+++ angular-frontend/kontrollrommet-frontend/src/app/meetings/meeting-newrequest.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'meeting-newrequest',
+    templateUrl: 'meeting-newrequest.component.html'
+})
+
+export class MeetingNewrequestComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+    
+}
