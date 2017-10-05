33aecf2d40b0d3b776c75285a723788a72ed9d66
Janic Heen <janic@online.no>
2017-10-05 11:12:24 +0200

    Folder and naming restructuring in progress. Here just moved files in new folders

--- kontrollrommet_website/client_views/serializers/__init__.py
+++
@@ -1,3 +0,0 @@
-from .current_user_serializers import *
-from .post_serializers import *
-from .query_paramater_serializers import *
--- kontrollrommet_website/process_control/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from process_control import views
-
-router = DefaultRouter()
-router.register(r'case', views.CaseViewSet)
-
-urlpatterns = router.urls
